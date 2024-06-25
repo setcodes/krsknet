107e2b8c2151ef9cc9bd3e82086833f1a0e8f882
Egor Maximov <maximov.devs@gmail.com>
2024-06-26 00:31:40 +0300

    out v1.1

--- src/next.config.mjs
+++ src/next.config.mjs
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export',
-	// assetPrefix: isProd ? '/' : undefined,
+	assetPrefix: isProd ? 'https://setcodes.github.io/krsknet' : undefined,
 	webpack(config) {
 		config.module.rules.push({
 			test: /\.svg$/,
